3f5c1a8e863d7befb9fbcfda5ef720657c9a1643
chintu-srinu <budigesrinivas885@gmail.com>
2025-03-05 11:00:45 +0530

    Add files via upload

---
+++ 24.01/data.py
@@ -0,0 +1,23 @@
+# Interger
+a=10+10
+print(a)
+# float
+a=2.5
+b=9.2
+print(a+b)
+
+# Array
+a=[1,2,3,4,1,2,3]
+b=[1,2,3,4,1,2,3]
+print(a+b)
+
+# list
+
+a=[1,2,3,4,5]
+b=[6,7,8,1,2]
+print(a+b)
+
+# tuple
+a=(1,2,3,4,"srinu","true")
+print(type(a))
+print(a)

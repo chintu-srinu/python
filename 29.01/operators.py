17b32d6cdebd06038f88ca928ea08351a771fd47
chintu-srinu <budigesrinivas885@gmail.com>
2025-03-05 11:01:21 +0530

    Add files via upload

---
+++ 29.01/operators.py
@@ -0,0 +1,19 @@
+
+# And operator
+a=10
+b=20
+print(a>=10 and b<30)
+
+# or operator
+a=10
+b=20
+print(a>=10 or b<=20)
+# not operator
+a=10
+b=20 
+print(not(a>b))
+
+# Not
+flag = False
+flag = not flag
+print(flag)  
